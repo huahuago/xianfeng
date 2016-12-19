7045d31ffcff80d92a9ee054b1650877b8404464
liuyahua <907551523@qq.com>
2016-12-19 17:37:26 +0800

    ”提交微信相

--- main.php
+++ main.php
@@ -1,3 +1,9 @@
+<?php
+require_once "jssdk.php";
+// appId  和 秘钥
+$jssdk = new JSSDK("wx71619156de973afc", "1816136d02ca31a98e3e7071204f3672");
+$signPackage = $jssdk->GetSignPackage();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -111,15 +117,74 @@
             	我的
         	</a>
         </li>
-   </ul>
-    
-    
-    
+   </ul>   
 </body>
 <script type="text/javascript">
     document.documentElement.style.fontSize = innerWidth/4.14 +"px";
     window.onresize =function(){
       document.documentElement.style.fontSize = innerWidth/4.14 + "px";
     }
+</script>
+<script>
+
+  wx.config({
+    debug: true,
+    appId: '<?php echo $signPackage["appId"];?>',
+    timestamp: <?php echo $signPackage["timestamp"];?>,
+    nonceStr: '<?php echo $signPackage["nonceStr"];?>',
+    signature: '<?php echo $signPackage["signature"];?>',
+     jsApiList: [
+        'checkJsApi',
+        'onMenuShareWeibo',
+        'onMenuShareQZone',
+        'hideMenuItems',
+        'showMenuItems',
+        'hideAllNonBaseMenuItem',
+        'showAllNonBaseMenuItem',
+        'translateVoice',
+        'startRecord',
+        'stopRecord',
+        'onVoiceRecordEnd',
+        'playVoice',
+        'onVoicePlayEnd',
+        'pauseVoice',
+        'stopVoice',
+        'uploadVoice',
+        'downloadVoice',
+        'chooseImage',
+        'previewImage',
+        'uploadImage',
+        'downloadImage',
+        'getNetworkType',
+        'openLocation',
+        'getLocation',
+        'hideOptionMenu',
+        'showOptionMenu',
+        'closeWindow',
+        'scanQRCode',
+        'chooseWXPay',
+        'openProductSpecificView',
+        'addCard',
+        'chooseCard',
+        'openCard'
+      ]
+  });
+  wx.ready(function () {
+    // 在这里调用 API
+
+    wx.getLocation({
+    success: function (res) {
+        var latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
+        var longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
+        var speed = res.speed; // 速度，以米/每秒计
+        var accuracy = res.accuracy; // 位置精度
+    },
+    cancel: function (res) {
+        alert('用户拒绝授权获取地理位置');
+    }
+});
+
+
+  });
 </script>
 </html>
